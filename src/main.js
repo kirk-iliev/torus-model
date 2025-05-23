4e12695af9527391b6b494411f077f29ecce6551
Kirk Iliev <kirkiliev0@gmail.com>
2025-05-22 17:09:12 -0700

    node env changes idk

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  90, window.innerWidth / window.innerHeight, 0.1, 1000
+);
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize( window.innerWidth, window.innerHeight );
