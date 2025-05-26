0b687a767a3f2dfb512ea6cb75e85419c4e6d7cc
Kirk Iliev <kirkiliev0@gmail.com>
2025-05-25 17:26:35 -0700

    Refactor and add comments

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 const gui = new dat.GUI();
 
+// Default Torus parameters
 const settings = {
   rotationSpeed: 1,
   radius: 1,
@@ -15,6 +16,7 @@ const settings = {
   tubularSegments: 40
 };
 
+// Uniforms for shaders
 const uniforms = {
   u_time: { value: 0 },
   u_radius: { value: settings.radius },
@@ -49,6 +51,30 @@ const fragmentShader = `
     gl_FragColor = vec4(0.0, 1.0, 0.4, 1.0);
 }`;
 
+// dat.gui controls
+const controllers = {
+  rotationSpeed: gui.add(settings, 'rotationSpeed', 0.1, 5, 0.01),
+  radius: gui.add(settings, 'radius', 0.1, 5, 0.005).onChange(() => {
+    Torus.update();
+    uniforms.u_radius.value = settings.radius;
+  }),
+  tube: gui.add(settings, 'tube', 0.1, 5, 0.005).onChange(() => {
+    Torus.update();
+    uniforms.u_tube.value = settings.tube;
+  }),
+  radialSegments: gui.add(settings, 'radialSegments', 3, 100).onChange(() => {
+    Torus.update();
+  }),
+  tubularSegments: gui.add(settings, 'tubularSegments', 3, 100).onChange(() => {
+    Torus.update();
+  }),
+  rotateX: gui.add(settings, 'rotateX'),
+  rotateY: gui.add(settings, 'rotateY'),
+  rotateZ: gui.add(settings, 'rotateZ'),
+  flowAnimation: gui.add(settings, 'flowAnimation'),
+}
+
+// Reset the whole simulation
 const reset = {
   resetTorus: () => {
     settings.rotationSpeed = 1;
@@ -75,28 +101,7 @@ const reset = {
   }
 }
 
-const controllers = {
-  rotationSpeed: gui.add(settings, 'rotationSpeed', 0.1, 5, 0.01),
-  radius: gui.add(settings, 'radius', 0.1, 5, 0.005).onChange(() => {
-    Torus.update();
-    uniforms.u_radius.value = settings.radius;
-  }),
-  tube: gui.add(settings, 'tube', 0.1, 5, 0.005).onChange(() => {
-    Torus.update();
-    uniforms.u_tube.value = settings.tube;
-  }),
-  radialSegments: gui.add(settings, 'radialSegments', 3, 100).onChange(() => {
-    Torus.update();
-  }),
-  tubularSegments: gui.add(settings, 'tubularSegments', 3, 100).onChange(() => {
-    Torus.update();
-  }),
-  rotateX: gui.add(settings, 'rotateX'),
-  rotateY: gui.add(settings, 'rotateY'),
-  rotateZ: gui.add(settings, 'rotateZ'),
-  flowAnimation: gui.add(settings, 'flowAnimation'),
-}
-
+// Reset camera function
 const resetCamera = {
   resetCamera: () => {
   camera.position.set(0, 4, 0);
@@ -108,9 +113,10 @@ const resetCamera = {
 gui.add(reset, 'resetTorus').name('Click here to reset simulation')
 gui.add(resetCamera, 'resetCamera').name('Click here to reset Camera');
 
-
+// Update all controllers to reflect settings
 Object.values(controllers).forEach(controller => controller.updateDisplay());
 
+// Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   90, window.innerWidth / window.innerHeight, 0.1, 1000
@@ -120,6 +126,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Torus Object
 const Torus = {
   mesh: null,
 
@@ -142,6 +149,7 @@ const Torus = {
     scene.add(this.mesh);
   },
 
+  // Update the Torus geometry based on settings
   update() {
     const newGeometry = new THREE.TorusGeometry(
       settings.radius,
@@ -153,16 +161,18 @@ const Torus = {
     this.mesh.geometry.dispose();
     this.mesh.geometry = newGeometry;
   },
-
+  // Reset the Torus to default position and rotation
   resetTransform() {
     this.mesh.position.set( 0, 0, 0)
     this.mesh.rotation.set( 0, 0, 0);
   }
 };
 
+// Create the Torus object
 Torus.create();
 camera.position.y = 4;
 
+// Main animation loop
 function animate() {
 
   settings.rotateX && (Torus.mesh.rotation.x += settings.rotationSpeed * 0.01);
